94548119b227da7d54ffb24cf0286fe7527225a7
mubashir <49488919+mubaxhir@users.noreply.github.com>
2019-12-14 15:23:18 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,34 +5,33 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://mubi:1234@cluster001-avto2.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
-@app.route('/retrieve_list',methods=['GET'])
+@app.route('/todo/api/v1.0/tasks',methods=['GET'])
 def retrieve_list():
     userData = mongo.db.userData
     print(type(userData))
     output = []
     for q in userData.find():
-        output.append({'id' : q['id'], 'Title' : q['Title'],'Description':q['Description']})
+        output.append({'id' : q['id'], 'Title' : q['Title'],'Description':q['Description'],"Done":q["Done"]})
     return jsonify({'result' : output})
 
-@app.route('/retrieve',methods=['POST','GET'])
-def retrieve():
-    data = dict(request.json)
+@app.route('/todo/api/v1.0/tasks/<id>',methods=['GET'])
+def retrieve(id):
     try:
-        data["id"] = int(data["id"])
+        id = int(id)
     except:
         return jsonify({"not succesful":"id must be numeric"})
     else:
         userData = mongo.db.userData
-        result = userData.find_one({"id":data["id"]})
+        result = userData.find_one({"id":id})
         output=[]
-        output.append({'Description':result['Description'], 'Title' : result['Title'],'id':result['id']})
+        output.append({'Done':result['Done'],'Description':result['Description'], 'Title' : result['Title'],'id':result['id']})
         print(output)
         if result:
             return jsonify({"output":output})
         else:
             return jsonify({"unsuccessful":"id not found"}) 
 
-@app.route('/create',methods=['GET','POST'])
+@app.route('/todo/api/v1.0/tasks',methods=['POST','GET'])
 def create():
     data = request.json
     data = dict(data)
@@ -49,11 +48,15 @@ def create():
             userData.insert_one(data)
             return jsonify({"success":True})
 
-@app.route('/update',methods=['POST','PUT'])
-def update():
+@app.route('/todo/api/v1.0/tasks/<id>',methods=['PUT'])
+def update(id):
     data = dict(request.json)
+    try:
+        id = int(id)
+    except:
+        return jsonify({"not succesful":"id must be numeric"})
     userData = mongo.db.userData
-    result = userData.find_one({"id":data["id"]})
+    result = userData.find_one({"id":id})
     if result:
         userData.delete_one({"id": data["id"]})
         userData.insert_one(data)
@@ -61,20 +64,17 @@ def update():
     else:
         return jsonify({"id not found":True})
  
-@app.route('/delete',methods =['POST','DELETE'])
-def delete():
-    data = dict(request.json)
+@app.route('/todo/api/v1.0/tasks/<id>',methods =['DELETE'])
+def delete(id):
     userData = mongo.db.userData
-    result = userData.find_one({"id":data["id"]})
-    if data["id"] == result["id"]:
-        userData.delete_one({"id": data["id"]})
+    result = userData.find_one({"id":id})
+    if id == result["id"]:
+        userData.delete_one({"id": id})
         return jsonify({"deleted":True})
     else:
         return jsonify({"deleted":False})
 
 if __name__ ==  "__main__":
     app.run(debug=True)
-
-
 
 # Task 1 DONE!!!!
